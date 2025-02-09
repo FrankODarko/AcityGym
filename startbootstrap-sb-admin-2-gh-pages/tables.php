23fb149960dc287f68a19e9fdccc140d629407d9
FrankODarko <148505476+FrankODarko@users.noreply.github.com>
2025-02-09 22:32:26 +0000

    changed database connection in tables.php

--- startbootstrap-sb-admin-2-gh-pages/tables.php
+++ startbootstrap-sb-admin-2-gh-pages/tables.php
@@ -422,13 +422,16 @@
                                     </tfoot>
                                     <tbody>
                                         <?php
-                                        $host = 'localhost';
-                                        $user = 'root';
-                                        $password = '';
-                                        $database = 'newytest';
+                                        // Get JAWSDB connection information
+                                        $jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+                                        $jawsdb_server = $jawsdb_url["host"];
+                                        $jawsdb_username = $jawsdb_url["user"];
+                                        $jawsdb_password = $jawsdb_url["pass"];
+                                        $jawsdb_db = substr($jawsdb_url["path"], 1);
 
-                                        $conn = new mysqli($host, $user, $password, $database);
-
+                                        // Connect to DB
+                                        $conn = new mysqli($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+                                        
                                         if (!$conn) {
                                             die("Connection failed: " . $conn->connect_error);
                                         }
