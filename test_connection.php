1bb6677999c776065290f3de6d924acac792454f
FrankODarko <148505476+FrankODarko@users.noreply.github.com>
2025-02-09 21:58:56 +0000

    added test_connection_php file to test if im getting access to database

---
+++ test_connection.php
@@ -0,0 +1,20 @@
+<?php
+// Get JAWSDB connection information
+$jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+$jawsdb_server = $jawsdb_url["host"];
+$jawsdb_username = $jawsdb_url["user"];
+$jawsdb_password = $jawsdb_url["pass"];
+$jawsdb_db = substr($jawsdb_url["path"], 1);
+
+// Connect to DB
+$conn = new mysqli($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database!";
+}
+
+$conn->close();
+?>
